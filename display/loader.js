51ae922b361cfc0f986b57fa2056d203f419e0c4
ehiggins98 <ericdhiggins@gmail.com>
2020-02-06 23:01:48 -0600

    Code for display website

---
+++ display/loader.js
@@ -0,0 +1,27 @@
+const baseUrl = "http://hi.sandy"
+let loaded = 0
+
+function getNames() {
+	let request = new XMLHttpRequest()
+	request.addEventListener("load", () => {
+		console.log(request.status)
+		if (request.status != 200) {
+			return
+		}
+		let names  = JSON.parse(request.response)
+		
+		while (loaded < names.length) {
+			let name = document.createElement('p')
+			let text = document.createTextNode(names[loaded])
+			name.appendChild(text)
+			document.getElementById('text-box').appendChild(name)
+			loaded++;
+		}
+	})
+	request.open('GET', baseUrl)
+	request.send()	
+
+}
+
+getNames()
+setInterval(getNames, 1000)
